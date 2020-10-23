82031b819a5a551ea44905ad05d5792e67d8e6c3
Himanshu Kumar <48569211+Himanshu396@users.noreply.github.com>
2020-10-23 13:26:02 +0530

    Add files via upload
    hello world program

---
+++ himanshu.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = 84.3334
+	fmt.Printf("Hello,HIMANSHU kumar")
+	fmt.Println(x)
+	y := 42.333
+	fmt.Println(y)
+	fmt.Printf("y is of type %T", y)
+
+	var p, q, r, s = 3, 4, "ram", 1.2
+	fmt.Println(p)
+	fmt.Println(q)
+	fmt.Printf(r)
+	fmt.Println(s)
+
+}

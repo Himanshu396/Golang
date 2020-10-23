99131f4912b1d56ce00365a6fecc5dc1b097d82a
Himanshu Kumar <48569211+Himanshu396@users.noreply.github.com>
2020-10-23 13:37:57 +0530

    variable and datatypes
    variable and datatypes

---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num = 160
+	num1 := "himanshu"
+	fmt.Printf("%T   ", num)
+
+	fmt.Printf("%T", num1)
+}
---
+++ variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 160
+	num = num + 60
+	fmt.Println(num)
+}

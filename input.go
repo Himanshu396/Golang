764efa06443ce50378ea3b6068d2730d5152a32c
Himanshu Kumar <48569211+Himanshu396@users.noreply.github.com>
2020-11-27 11:56:35 +0530

    Update input.go

--- input.go
+++ input.go
@@ -4,13 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("type something:  ")
 	scanner.Scan()
-	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	input := scanner.Text()
+	//input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 	fmt.Printf("you typed: %q", input)
 }
